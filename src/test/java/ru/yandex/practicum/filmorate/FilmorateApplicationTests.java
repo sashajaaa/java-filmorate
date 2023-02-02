99e7de25847192041697f15ab842ab4decc0472f
sashajaaa <sashajaaa@yandex.ru>
2023-02-02 15:16:51 +0300

    Коммит №9.2: refactor: -Поправлен класс FilmorateApplicationTests.

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,13 +1,11 @@
-package java.ru.yandex.practicum.filmorate;
+package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FilmorateApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
