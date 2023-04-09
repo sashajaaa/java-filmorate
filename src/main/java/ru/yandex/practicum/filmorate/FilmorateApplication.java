df7c36561b769688325c22629c162493150b8655
PopkBot <themrgenich@mail.ru>
2023-04-09 13:59:09 +0300

    refactor: заменить вывод полей в SQL запросах: было *, стало films.*, rating_mpa.*,film_genres.* refactor: исправить Search films anyway by upDatE

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -9,3 +9,19 @@ public class FilmorateApplication {
 		SpringApplication.run(FilmorateApplication.class, args);
 	}
 }
+
+/*DROP TABLE IF EXISTS film_genres;
+DROP TABLE IF EXISTS friends;
+DROP TABLE IF EXISTS likes;
+DROP TABLE IF EXISTS genres;
+DROP TABLE IF EXISTS un_review_film;
+DROP TABLE IF EXISTS un_review_user;
+DROP TABLE IF EXISTS un_reviews_user_film;
+DROP TABLE IF EXISTS un_user_likes_review;
+DROP TABLE IF EXISTS un_user_dislikes_review;
+DROP TABLE IF EXISTS reviews;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS films_directors;
+DROP TABLE IF EXISTS directors;
+DROP TABLE IF EXISTS films;
+DROP TABLE IF EXISTS rating_mpa;*/
